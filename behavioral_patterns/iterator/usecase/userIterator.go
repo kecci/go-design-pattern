5af25a18af50950b6acc9b73c029864c542cdb71
kecci <kecci@machtwatch.co.id>
2021-02-22 14:37:06 +0700

    fix lint

--- behavioral_patterns/iterator/usecase/userIterator.go
+++ behavioral_patterns/iterator/usecase/userIterator.go
@@ -1,22 +1,18 @@
 package usecase
 
 type UserIterator struct {
-    Index int
-    Users []*User
+	Index int
+	Users []*User
 }
 
 func (u *UserIterator) HasNext() bool {
-    if u.Index < len(u.Users) {
-        return true
-    }
-    return false
-
+	return u.Index < len(u.Users)
 }
 func (u *UserIterator) GetNext() *User {
-    if u.HasNext() {
-        user := u.Users[u.Index]
-        u.Index++
-        return user
-    }
-    return nil
+	if u.HasNext() {
+		user := u.Users[u.Index]
+		u.Index++
+		return user
+	}
+	return nil
 }
